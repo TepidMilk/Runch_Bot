a6ad95f17f115eed7a3ec1ffce6e600fd450bf4b
TepidMilk <ivorykingrey@gmail.com>
2025-03-18 14:36:35 -0500

    Add homie_point_cog

---
+++ cogs/homie_point_cog.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+from data.graph import HomiePointsGraph
+
+homie = HomiePointsGraph()
+
+class HomiePointCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, msg):
+        if msg.content == "ping homie-cog":
+            await msg.channel.send("homie-cog is online...", delete_after=3)
+
+    @commands.command()
+    async def add_debt(self, ctx, to_user: discord.Member, points=1):
+        homie.add_debt(ctx.author.id, to_user.id, points)
+        score = homie.get_score(ctx.author.id, to_user.id)
+        await ctx.send(f"{ctx.author.nick} {score[0]} - {to_user.nick} {score[1]}")
+        
