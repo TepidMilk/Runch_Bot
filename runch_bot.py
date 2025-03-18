7904a702955b783759acbff60ab93e52865e6636
TepidMilk <ivorykingrey@gmail.com>
2025-03-18 12:16:06 -0500

    Update runch_bot
    set bot token and log in

--- runch_bot.py
+++ runch_bot.py
@@ -1,8 +1,19 @@
 import discord
+import os
 from discord.ext import commands
 from dotenv import load_dotenv
 
+#Load token from .env
+load_dotenv(".env")
+TOKEN: str = os.getenv("TOKEN")
+
+#set bot intents. Allows messages, reactions, access to member info and emojis and stickers
 intents = discord.Intents(message_content=True, reactions=True, members=True, emojis_and_stickers=True)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-client = discord.Client(intents=intents)
+@bot.event
+async def on_ready():
+    print("Bot is online...")
+    print(f"Logged in as {bot.user}...")
 
+bot.run(TOKEN)
