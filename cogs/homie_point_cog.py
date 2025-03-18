b61cf3ca223f28b97815cad332a57a1625498d86
TepidMilk <ivorykingrey@gmail.com>
2025-03-18 16:35:13 -0500

    Add get_total_owed command and Fix settle_debt
    response handling on settle_debt checks for
    response content now. Previously checked object

--- cogs/homie_point_cog.py
+++ cogs/homie_point_cog.py
@@ -22,21 +22,35 @@ class HomiePointCog(commands.Cog):
         
     @commands.command()
     async def settle_debt(self, ctx, to_user: discord.Member, points=0):
-        score = homie.get_score(ctx.author.id, to_user.id)
         await ctx.send(f"{to_user.mention},\n Has {ctx.author.global_name} sufficiently paid their Homie Points you (y/n)")
 
         def check(msg):
-            return msg.author == to_user and (msg.content == "y" or msg.content == "n")
+            return msg.author.id == to_user.id and (msg.content == "y" or msg.content == "n")
 
         try:
             response = await self.bot.wait_for("message", timeout=60.0, check=check)
         except asyncio.TimeoutError:
             await ctx.send("Confirmation timed out. The Debt was not settled")
 
-        if response == "y":
+        if response.content == "y":
             homie.settle_debt(ctx.author.id, to_user.id, points)
+            score = homie.get_score(ctx.author.id, to_user.id)
             await ctx.send(f"{ctx.author.global_name} {score[0]} - {to_user.global_name} {score[1]}")
-            settlement = await ctx.send(f"{ctx.author.global_name} has settled their debt of {points} homie points to {to_user.global_name}")
+            if points == 0:
+                settlement = await ctx.send(f"{ctx.author.global_name} has settled their debt with {to_user.global_name}")
+            else:
+                settlement = await ctx.send(f"{ctx.author.global_name} has settled their debt of {points} homie points to {to_user.global_name}")
             await settlement.add_reaction("🤝")
-        elif response == "n":
+        elif response.content == "n":
             await ctx.send("The Debt has not been settled.")
+
+    @commands.command()
+    async def get_total_owed(self, ctx):
+        await ctx.send(f"Total: {homie.get_total_owed(ctx.author.id)}")
+        for id in homie.graph[ctx.author.id]:
+            to_user: discord.User = self.bot.get_user(id)
+            owed = homie.graph[ctx.author.id][id]
+            await ctx.send(f"{to_user.global_name} owes you {owed} Homie Points")
+
+    
+            
