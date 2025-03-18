3beef2f4f4ca73edaa572df5e524964408f850b5
TepidMilk <ivorykingrey@gmail.com>
2025-03-17 20:18:14 -0500

    Update runch_bot
    add intents and client

--- runch_bot.py
+++ runch_bot.py
@@ -1,3 +1,8 @@
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+
+intents = discord.Intents(message_content=True, reactions=True, members=True, emojis_and_stickers=True)
+
+client = discord.Client(intents=intents)
+
