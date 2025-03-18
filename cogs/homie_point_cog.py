a367a23b0397a3917d015cda53933e7c03247974
TepidMilk <ivorykingrey@gmail.com>
2025-03-18 16:04:44 -0500

    Add settle_debt command

--- cogs/homie_point_cog.py
+++ cogs/homie_point_cog.py
@@ -1,4 +1,5 @@
 import discord
+import asyncio
 from discord.ext import commands
 from data.graph import HomiePointsGraph
 
@@ -19,3 +20,23 @@ class HomiePointCog(commands.Cog):
         score = homie.get_score(ctx.author.id, to_user.id)
         await ctx.send(f"{ctx.author.global_name} {score[0]} - {to_user.global_name} {score[1]}")
         
+    @commands.command()
+    async def settle_debt(self, ctx, to_user: discord.Member, points=0):
+        score = homie.get_score(ctx.author.id, to_user.id)
+        await ctx.send(f"{to_user.mention},\n Has {ctx.author.global_name} sufficiently paid their Homie Points you (y/n)")
+
+        def check(msg):
+            return msg.author == to_user and (msg.content == "y" or msg.content == "n")
+
+        try:
+            response = await self.bot.wait_for("message", timeout=60.0, check=check)
+        except asyncio.TimeoutError:
+            await ctx.send("Confirmation timed out. The Debt was not settled")
+
+        if response == "y":
+            homie.settle_debt(ctx.author.id, to_user.id, points)
+            await ctx.send(f"{ctx.author.global_name} {score[0]} - {to_user.global_name} {score[1]}")
+            settlement = await ctx.send(f"{ctx.author.global_name} has settled their debt of {points} homie points to {to_user.global_name}")
+            await settlement.add_reaction("🤝")
+        elif response == "n":
+            await ctx.send("The Debt has not been settled.")
