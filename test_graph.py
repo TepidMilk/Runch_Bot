07a78d751b216a2315c10ecf14914cae07e1a3ac
TepidMilk <ivorykingrey@gmail.com>
2025-03-17 20:24:39 -0500

    Add test_graph

---
+++ test_graph.py
@@ -0,0 +1,8 @@
+import unittest
+from graph import HomiePointsGraph
+
+class GraphTest(unittest.TestCase):
+    def test_add_user(self):
+        homie_graph = HomiePointsGraph()
+        homie_graph.add_user("Noah")
+        self.assertAlmostEqual(homie_graph.graph, {"Noah": {}})
