bcc27d155556d000fde732206ae1d330e85c6378
TepidMilk <ivorykingrey@gmail.com>
2025-03-19 10:23:43 -0500

    Update command names and fix list_debt, list_owed
    list debt and list_owed now send embeds with
    avatar icons

--- cogs/homie_point_cog.py
+++ cogs/homie_point_cog.py
@@ -15,7 +15,7 @@ class HomiePointCog(commands.Cog):
             await msg.channel.send("homie-cog is online...", delete_after=3)
 
     @commands.command()
-    async def add_debt(self, ctx, to_user: discord.Member, points=1):
+    async def give_points(self, ctx, to_user: discord.Member, points=1):
         homie.add_debt(ctx.author, to_user, points)
         score = homie.get_score(ctx.author, to_user)
         await ctx.send(f"{ctx.author.global_name} {score[0]} - {to_user.global_name} {score[1]}")
@@ -45,18 +45,26 @@ class HomiePointCog(commands.Cog):
             await ctx.send("The Debt has not been settled.")
 
     @commands.command()
-    async def get_total_owed(self, ctx):
-        await ctx.send(f"Total: {homie.get_total_owed(ctx.author)}")
-        for user in homie.graph[ctx.author]:
-            owed = homie.graph[ctx.author][user]
-            await ctx.send(f"{user.global_name} owes you {owed} Homie Points")
+    async def list_owed(self, ctx):
+        owed_embed = discord.Embed()
+        owed_embed.set_author(name=f"{ctx.author.global_name}", icon_url=ctx.author.display_avatar.url)
+        owed_embed.add_field(name=f"Total: {homie.get_total_owed(ctx.author)}", value="", inline=False)
+        value = []
+        for user in homie.graph:
+            if ctx.author in homie.graph[user]:
+                value.append(f"{user.global_name}: {homie.graph[user][ctx.author]}")
+        owed_embed.add_field(name="", value="\n".join(value), inline=False)
+        await ctx.send(embed=owed_embed)
 
     @commands.command()
-    async def get_debt(self, ctx):
+    async def list_debt(self, ctx):
         debt_embed = discord.Embed(title=None)
+        debt_embed.set_author(name=f"{ctx.author.global_name}'s Debts", icon_url=ctx.author.display_avatar.url)
         debts = homie.get_debt(ctx.author)
+        value = []
         for user in debts:
-            debt_embed.add_field(name=f"{user.global_name}:", value=f"{debts[user]}")
+            value.append(f"{user.global_name}: {debts[user]}")
+        debt_embed.add_field(name="", value="\n".join(value), inline=False)
         await ctx.send(embed=debt_embed)
     
             
