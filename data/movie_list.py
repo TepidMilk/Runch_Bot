5a5600047b945d89405932b949453081494e8474
TepidMilk <ivorykingrey@gmail.com>
2025-03-20 14:40:54 -0500

    Update movie_list to reflect new movie sundays

--- data/movie_list.py
+++ data/movie_list.py
@@ -2,29 +2,23 @@ class MovieList():
     def __init__(self):
         self.movies = {}
 
-    def add_user(self, user_node):
+    """def add_user(self, user_node):
         if user_node not in self.movies:
-            self.movies[user_node] = set()
+            self.movies[user_node] = None"""
 
     def add_movie(self, user_node, movie: str):
         if user_node not in self.movies:
-            self.add_user(user_node)
-        self.movies[user_node].add(movie.upper())
+            self.movies[user_node] = movie
+        else:
+            return
+        
     
-    def remove_movie(self, user_node, movie: str):
-        self.movies[user_node].remove(movie.upper())
+    def remove_movie(self, user_node):
+        del self.movies[user_node]
 
-    def remove_winner(self, movie: str):
-        for user in self.movies:
-            if movie.upper() in self.movies[user]:
-                self.movies[user].remove(movie.upper())
-
-    def get_movie_list(self, user_node):
-        return self.movies[user_node]
+    def reset_list(self):
+        self.movies = {}
     
     def get_all_movies(self):
-        all_movies = []
-        for user in self.movies:
-            all_movies.extend(self.movies[user])
-        return all_movies
+        return self.movies.items()
     
