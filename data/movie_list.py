9ce08d2adc41b67f8ac29a271815c9a461d0478b
TepidMilk <ivorykingrey@gmail.com>
2025-03-19 19:01:53 -0500

    Add methods
    remove_movie, remove_winner, get_movie_list,
    get_all_movies

--- data/movie_list.py
+++ data/movie_list.py
@@ -3,10 +3,28 @@ class MovieList():
         self.movies = {}
 
     def add_user(self, user_node):
-        if user_node not in self.graph:
+        if user_node not in self.movies:
             self.movies[user_node] = set()
 
     def add_movie(self, user_node, movie: str):
         if user_node not in self.movies:
             self.add_user(user_node)
         self.movies[user_node].add(movie.upper())
+    
+    def remove_movie(self, user_node, movie: str):
+        self.movies[user_node].remove(movie.upper())
+
+    def remove_winner(self, movie: str):
+        for user in self.movies:
+            if movie.upper() in self.movies[user]:
+                self.movies[user].remove(movie.upper())
+
+    def get_movie_list(self, user_node):
+        return self.movies[user_node]
+    
+    def get_all_movies(self):
+        all_movies = []
+        for user in self.movies:
+            all_movies.extend(self.movies[user])
+        return all_movies
+    
