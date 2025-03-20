92a9fa4ee5e15055cb6accec5c760d34b75f48a5
TepidMilk <ivorykingrey@gmail.com>
2025-03-20 15:11:04 -0500

    Update movie_list_cog
    use pickle for storage and update to reflect new
    movie_list commands and changes

--- cogs/movie_list_cog.py
+++ cogs/movie_list_cog.py
@@ -1,10 +1,20 @@
 import discord
 import asyncio
-from discord.ext import commands
+import pickle
+import os
+from discord.ext import commands, tasks
 from data.movie_list import MovieList
 import random
 
-movies_list = MovieList()
+def save_movie_list(movies, filename="movie_data.pkl"):
+    with open(filename, "wb") as file:
+        pickle.dump(movies, file)
+
+def load_movie_list(filename="movie_data.pkl"):
+    if os.path.exists(filename):
+        with open(filename, "rb") as file:
+            return pickle.load(file)
+    return MovieList()
 
 def is_channel(ctx):
     return (ctx.channel.id == 1346252701159129328 or ctx.channel.id == 1351608689550688328)
@@ -12,6 +22,10 @@ def is_channel(ctx):
 class MovieListCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.movies = load_movie_list()
+
+    def save_movies(self):
+        save_movie_list(self.movies)
 
     @commands.Cog.listener()
     async def on_message(self, msg):
@@ -21,37 +35,34 @@ class MovieListCog(commands.Cog):
     @commands.command()
     @commands.check(is_channel)
     async def add_movie(self, ctx, movie):
-        movies_list.add_movie(ctx.author, movie)
-        await ctx.send(f"{ctx.author.global_name} added {movie} to their list")
+        msg = self.movies.add_movie(ctx.author, movie)
+        self.save_movies()
+        if msg == None:
+            await ctx.send(f"{ctx.author.global_name} added {movie} to their list")
+        else:
+            await ctx.send(msg)
 
     @commands.command()
     @commands.check(is_channel)
-    async def remove_movie(self, ctx, movie):
-        movies_list.remove_movie(ctx.author, movie)
-        await ctx.send(f"{ctx.author.global_name} removed {movie} from their list")
+    async def remove_movie(self, ctx):
+        self.movies.remove_movie(ctx.author)
+        self.save_movies()
+        await ctx.send(f"{ctx.author.global_name} removed their movie")
 
     @commands.command()
     @commands.check(is_channel)
     async def list_movies(self, ctx):
         list_embed = discord.Embed()
-        list_embed.set_author(name=f"{ctx.author.global_name}'s Movie List", icon_url=ctx.author.display_avatar.url)
-        list_embed.add_field(name="", value="\n".join(movies_list.get_movie_list(ctx.author)), inline=False)
-        await ctx.send(embed=list_embed)
-
-    @commands.command()
-    @commands.check(is_channel)
-    async def list_all_movies(self, ctx):
-        list_embed = discord.Embed()
-        list_embed.set_author(name="Movie List", icon_url=self.bot.user.display_avatar.url)
-        list_embed.add_field(name="", value="\n".join(movies_list.get_all_movies()), inline=False)
+        list_embed.set_author(name="The Kingdom's Movie List", icon_url=self.bot.user.display_avatar.url)
+        list_embed.add_field(name="\n".join(self.movies.get_all_movies()), value="", inline=False)
         await ctx.send(embed=list_embed)
 
     @commands.command()
     @commands.check(is_channel)
     async def movie_poll(self, ctx):
         movie_poll = discord.Poll(question="Vote for your Favorite Movie!", duration=1)
-        for user in movies_list.movies:
-            answer = random.choice(movies_list.movies[user])
+        for user in self.movies.movies:
+            answer = random.choice(self.movies.movies[user])
             movie_poll.add_answer(str(answer))
         await ctx.send(poll=movie_poll)
 
